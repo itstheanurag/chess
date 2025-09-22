d84490de9eb824a2f84b9dc835e0b77c83f0c7d6
gaurav <gauravanurag36@gmail.com>
2025-09-22 20:09:55 +0530

    feat: inegrate prisma

--- backend/src/types/socket.d.ts
+++ backend/src/types/socket.d.ts
@@ -1,8 +1,7 @@
-// types/socket.d.ts
 import "socket.io";
 
 declare module "socket.io" {
   interface Socket {
-    user?: any; // replace `any` with a specific User type if needed
+    user?: any; 
   }
 }
